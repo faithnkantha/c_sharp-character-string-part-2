d92aed0822b0819cfa417c92296ffe6182b336ba
Faith nkatha <faithnkatha160@gmail.com>
2023-11-14 00:05:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class QuestionTwo
+{
+    static void Main()
+
+    {
+        char letter;
+
+
+        Console.WriteLine("Enter a number ,letter or special character");
+        letter = Convert.ToChar(Console.ReadLine());
+        if (char.IsLetter(letter))
+        {
+            if (char.IsUpper(letter))
+                Console.WriteLine($"{letter}is a capital letter");
+            else
+                Console.WriteLine($"{letter}is a small letter");
+        }
+        else
+            Console.WriteLine("it's not a letter");
+
+
+        
+    }
+}
